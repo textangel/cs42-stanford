2cb30cca28ea08ebd9d389ac95d015f3958fa947
textangel <textangel@pku.edu.cn>
2019-05-17 11:34:47 +0800

    updatE

--- Assignment3 Newsfeed/lib/utils.js
+++
@@ -1,8 +0,0 @@
-// render a 404 response
-exports.render404 = function(request, response) {
-  response.status(404);
-  response.render('not-found.html');
-};
-
-// no operation placeholder for empty callbacks
-exports.noOperation = function() {};
--- Assignment3 Newsfeed/public/js/main.js
+++
@@ -1,6 +0,0 @@
-(function(window, document, undefined) {
-
-  /* Render the MainView, which handles application logic. */
-  MainView.render($(document.body));
-
-})(this, this.document);
